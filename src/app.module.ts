c7ff027f37459b66ebe9b5e40528b263606216ce
chris-am <c.aranguizm@outlook.com>
2022-07-19 10:56:54 -0400

    aiming project to mongobd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import {Module} from "@nestjs/common";
+import {ConfigModule} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
 import {AuthModule} from "./auth/auth.module";
 
 @Module({
-  imports: [ AuthModule ],
+  imports: [ 
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath:[ '.env.development' ],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.URI_MONGODB)
+  ],
 })
 
 export class AppModule {}
