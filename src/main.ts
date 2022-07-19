4f6893586bc4f2176049923162ceddc499f402c4
chris-am <c.aranguizm@outlook.com>
2022-07-19 09:05:41 -0400

    adding exeption and time out to main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
+import {AllExceptionFilter} from "./common/filters/http-exception.filter";
+import {TimeOutInterceptor} from "./common/interceptors/timeout.interceptor";
 
 
 async function fastoodBackend() {
   const port = process.env.PORT || 3000;
-  const app = await NestFactory.create( AppModule )
+  const app = await NestFactory.create( AppModule );
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
   console.log('listing in port ===> ', port);
   await app.listen( port );
 }
