55019f1a5256c723e1ca8056729e15f0b00f493d
chris-am <c.aranguizm@outlook.com>
2022-07-20 22:15:06 -0400

    declaring user logic

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,22 @@
+import {IsEmail, IsNotEmpty, IsNumber, IsString} from "class-validator";
+
+export class UserDTO{
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly adress: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly phoneCode: string; //default: +569
+  @IsNotEmpty()
+  @IsNumber()
+  readonly phoneNumber: number;
+  @IsNotEmpty()
+  @IsString()
+  readonly password: string;
+}
