fc0949f18662bace114b06923f8a9a61d78b3012
chris-am <c.aranguizm@outlook.com>
2022-07-25 00:01:31 -0400

    declaring product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [ 
-    AuthModule,
     ConfigModule.forRoot({
       envFilePath:[ '.env.development' ],
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.URI_MONGODB),
-    UserModule
+    UserModule,
+    ProductModule,
+    AuthModule,
   ],
 })
 
