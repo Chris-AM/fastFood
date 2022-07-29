3a95280f87836d2c227cf799850b7d7aa5131d76
chris-am <c.aranguizm@outlook.com>
2022-07-29 15:35:46 -0400

    formating code

--- src/ingredient/ingredient.controller.ts
+++ src/ingredient/ingredient.controller.ts
@@ -1,34 +1,44 @@
-import { Controller, Post, Body, Delete, Param, Put, Get } from '@nestjs/common';
-import {IngredientDTO} from './dto/ingrefient.dto';
+import {
+  Controller,
+  Post,
+  Body,
+  Delete,
+  Param,
+  Put,
+  Get,
+} from '@nestjs/common';
+import { IngredientDTO } from './dto/ingrefient.dto';
 import { IngredientService } from './ingredient.service';
 
 @Controller('api/v1/ingredient')
 export class IngredientController {
-
-  constructor(private readonly ingredientService: IngredientService){}
+  constructor(private readonly ingredientService: IngredientService) {}
 
   @Post()
-  createIngredient(@Body() ingredientDto: IngredientDTO){
+  createIngredient(@Body() ingredientDto: IngredientDTO) {
     return this.ingredientService.createIngredient(ingredientDto);
   }
 
   @Get()
-  getAllIngredints(){
+  getAllIngredints() {
     return this.ingredientService.getAllIngredints();
   }
 
   @Get(':id')
-  getIngredientByID(@Param('id') id: string){
+  getIngredientByID(@Param('id') id: string) {
     return this.ingredientService.getIngredientByID(id);
   }
 
   @Put(':id')
-  updateIngredient(@Param('id') id: string, @Body() ingredientDto: IngredientDTO){
+  updateIngredient(
+    @Param('id') id: string,
+    @Body() ingredientDto: IngredientDTO,
+  ) {
     return this.ingredientService.updateIngredient(id, ingredientDto);
   }
 
   @Delete(':id')
-  deleteIngredient(@Param('id') id: string){
+  deleteIngredient(@Param('id') id: string) {
     return this.ingredientService.deleteIngredient(id);
   }
 }
--- src/ingredient/ingredient.service.ts
+++ src/ingredient/ingredient.service.ts
@@ -7,29 +7,32 @@ import { IngredientDTO } from './dto/ingrefient.dto';
 
 @Injectable()
 export class IngredientService {
-  constructor( 
+  constructor(
     @InjectModel(INGREDIENT.name)
-    private readonly model: Model<IIngredient>
-  ){}
+    private readonly model: Model<IIngredient>,
+  ) {}
 
-  async createIngredient(ingredientDto: IngredientDTO): Promise<IIngredient>{
+  async createIngredient(ingredientDto: IngredientDTO): Promise<IIngredient> {
     const newIngredient = new this.model(ingredientDto);
     return await newIngredient.save();
   }
 
-  async getAllIngredints(): Promise<IIngredient[]>{
+  async getAllIngredints(): Promise<IIngredient[]> {
     return await this.model.find();
   }
 
-  async getIngredientByID(id: string): Promise<IIngredient>{
+  async getIngredientByID(id: string): Promise<IIngredient> {
     return await this.model.findById(id);
   }
 
-  async updateIngredient(id: string, ingredientDto: IngredientDTO): Promise<IIngredient>{
-    return await this.model.findByIdAndUpdate(id, ingredientDto, {new: true});
+  async updateIngredient(
+    id: string,
+    ingredientDto: IngredientDTO,
+  ): Promise<IIngredient> {
+    return await this.model.findByIdAndUpdate(id, ingredientDto, { new: true });
   }
 
-  async deleteIngredient(id: string): Promise<IIngredient>{
+  async deleteIngredient(id: string): Promise<IIngredient> {
     return await this.model.findByIdAndDelete(id);
   }
 }
