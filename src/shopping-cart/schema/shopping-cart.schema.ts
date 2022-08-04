fcec0d8c68f61514aa64da762a9a8b636b2b9aa7
chris-am <c.aranguizm@outlook.com>
2022-08-03 23:28:22 -0400

    shopping cart schema now has time stamp and custom collection name

--- src/shopping-cart/schema/shopping-cart.schema.ts
+++ src/shopping-cart/schema/shopping-cart.schema.ts
@@ -1,26 +1,36 @@
 import * as mongoose from 'mongoose';
 
-export const ShoppingCartSchema = new mongoose.Schema({
-  menus: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'menus',
+export const ShoppingCartSchema = new mongoose.Schema(
+  {
+    menus: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'menus',
+      },
+    ],
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'products',
+      },
+    ],
+    user: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    ],
+    total: {
+      type: Number,
+      required: true,
     },
-  ],
-  products: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'products',
-    },
-  ],
-  user: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'users',
-    },
-  ],
-  total: {
-    type: String,
-    
   },
+  {
+    timestamps: true,
+    collection: 'ShoppingCart',
+  },
+);
+ShoppingCartSchema.method('toJSON', function () {
+  const { __v, ...object } = this.toObject();
+  return object;
 });
