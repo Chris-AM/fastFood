f6ea76e84be26b93e59337dcd84394978c17f1ef
Chris-am <c.aranguizm@outlook.com>
2022-08-15 16:57:30 -0400

    :fire: user wont show __v and _id

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -1,31 +1,34 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose'
+import { Document } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
 export type UsersDocument = Users & Document;
 
-@Schema({timestamps: true})
+@Schema({ timestamps: true })
 export class Users {
   @Prop({ unique: true, default: uuidv4 })
   id: string;
-  @Prop({required: true})
+  @Prop({ required: true })
   name: string;
-  @Prop({required: true, unique: true})
+  @Prop({ required: true, unique: true })
   email: string;
-  @Prop({required: true})
+  @Prop({ required: true })
   adress: string;
-  @Prop({default: '+569'})
+  @Prop({ default: '+569' })
   phoneCode: string;
-  @Prop({required: true})
+  @Prop({ required: true })
   phoneNumber: number;
-  @Prop({required: true})
-  password: string;  
+  @Prop({ required: true })
+  password: string;
   @Prop()
   avatar: string;
-  @Prop({default:['user']})
+  @Prop({ default: ['user'] })
   role: string[];
 }
 
-export const UsersSchema =
-  SchemaFactory.createForClass(Users);
+export const UsersSchema = SchemaFactory.createForClass(Users);
 
+UsersSchema.method('toJSON', function () {
+  const { __v, _id, ...object } = this.toObject();
+  return object;
+});
