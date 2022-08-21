8b28d7206765db7314fc11e607847a5c9ff76635
Chris-am <c.aranguizm@outlook.com>
2022-08-21 01:12:07 -0400

    :bug: solving error

--- src/providers/socket-provider.ts
+++ src/providers/socket-provider.ts
@@ -11,17 +11,17 @@ import {
     },
 })
 export class SocketProvider
-    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+   // implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-    afterInit(server: any) {
-        throw new Error('Method not implemented.');
-    }
+    // afterInit(server: any) {
+    //     throw new Error('Method not implemented.');
+    // }
 
-    handleConnection(client: any, ...args: any[]) {
-        throw new Error('Method not implemented.');
-    }
-    
-    handleDisconnect(client: any) {
-        throw new Error('Method not implemented.');
-    }
+    // handleConnection(client: any, ...args: any[]) {
+    //     throw new Error('Method not implemented.');
+    // }
+
+    // handleDisconnect(client: any) {
+    //     throw new Error('Method not implemented.');
+    // }
 }
