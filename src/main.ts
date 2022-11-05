8bfd572aa609ce0549ac411d69f0788ab7c58ca0
Chris-am <c.aranguizm@outlook.com>
2022-11-05 01:11:26 -0300

    :art: formating using lint hints in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import { ValidationPipe, VersioningType } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import * as cookieParser from "cookie-parser";
-import { AppModule } from "./app.module";
-import { AllExceptionFilter } from "./common/filters/http-exception.filter";
-import {LoggerInterceptor} from "./common/interceptors/logger.interceptor";
-import { TimeOutInterceptor } from "./common/interceptors/timeout.interceptor";
-
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { LoggerInterceptor } from './common/interceptors/logger.interceptor';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function fastoodBackend() {
   const port = process.env.PORT || 3000;
-  const app = await NestFactory.create( AppModule, {
-    cors: true
+  console.log('🚀 running on port ', port);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
   });
   app.enableVersioning({
     defaultVersion: '1',
@@ -21,20 +21,20 @@ async function fastoodBackend() {
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new TimeOutInterceptor());
   app.useGlobalInterceptors(new LoggerInterceptor());
-  app.useGlobalPipes(new ValidationPipe);
+  app.useGlobalPipes(new ValidationPipe());
   const options = new DocumentBuilder()
-  .setTitle('FastFood App')
-  .setDescription('MVP')
-  .setVersion('1.0.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('FastFood App')
+    .setDescription('MVP')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/api/docs', app, document, {
     swaggerOptions: {
-      filter: true
-    }
+      filter: true,
+    },
   });
-  await app.listen( port );
+  await app.listen(port);
 }
 
 fastoodBackend();
